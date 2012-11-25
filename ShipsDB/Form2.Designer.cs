89adffce252f1e3f899fa19ead409093cc8c6a13
Ben Glasser <soul_trip@hotmail.com>
2012-11-25 03:19:28 -0800

    Add rest of project

---
+++ ShipsDB/Form2.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ShipsDB
+{
+    partial class ConnectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(87, 14);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(416, 20);
+            this.userBox.TabIndex = 0;
+            this.userBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(87, 37);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(416, 20);
+            this.passwordBox.TabIndex = 1;
+            this.passwordBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.passwordBox_KeyPress);
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(40, 18);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(32, 13);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "User:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // ConnectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 71);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.userBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ConnectForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Connect to Database";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.ConnectForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ ShipsDB/Form2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace ShipsDB
+{
+    public partial class ConnectForm : Form
+    {
+        String user;
+
+        public String User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+        String password;
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        Form1 caller;
+
+
+        public ConnectForm(Form1 caller)
+        {
+            this.caller = caller;
+            user = "";
+            password = "";
+            InitializeComponent();
+        }
+
+        private void ConnectForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == "\r")
+            {
+                caller.User = userBox.Text;
+                passwordBox.Focus();
+            }
+        }
+
+        private void passwordBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.ToString() == "\r")
+            {
+                caller.User = userBox.Text;
+                caller.Password = passwordBox.Text;
+                caller.Enabled = true;
+                caller.Focus();
+                this.Close();
+            }
+        }
+    }
+}
